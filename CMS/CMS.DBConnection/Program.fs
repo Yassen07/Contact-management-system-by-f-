c16f250ec59544f3a6be44c5d82d0c4faf9399bc
Mohey Eldeen Maher <54249650+mohey-e-maher@users.noreply.github.com>
2024-12-06 03:40:44 +0200

    make successful connection

---
+++ CMS/CMS.DBConnection/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+open System.Data
+open Microsoft.Data.SqlClient
+open MySql.Data.MySqlClient
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+
+// create connection data to Database
+
+
+
+// insert data in the data base 
+
+
+
+
+let retrieveData () =
+    let connectionString = "Server=localhost;Database=tester;User Id=root;Password=;"
+    // connection done 
+    printfn "connection done"
+    try
+        use connection = new MySqlConnection(connectionString)
+        connection.Open()
+        printfn "Connection successful"
+        let query = "SELECT Id, Name, Age FROM SampleTable"
+        use command = new MySqlCommand(query, connection)
+        use reader = command.ExecuteReader()
+        while reader.Read() do
+            let id = reader.GetInt32(0)
+            let name = reader.GetString(1)
+            let age = reader.GetInt32(2)
+            printfn "Id: %d, Name: %s, Age: %d" id name age
+    with
+    | ex -> printfn "Error: %s" ex.Message
+
+retrieveData ()
+
+
+printfn "Data Selected successfully!"
+
+printfn "Hello from F#"
